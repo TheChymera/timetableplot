bec6a8a5e784e4e76a6bd0a1b9eadeaedecb22cc
Horea Christian <horea.christ@yandex.com>
2016-04-27 11:14:18 +0200

    first draft of complete timetable function

--- timeplot/history.py
+++ timeplot/history.py
@@ -5,6 +5,13 @@ import json
 import sys
 from datetime import *
 
+from timeplan import add_grey
+import matplotlib.cm as cm
+
+from matplotlib.ticker import MultipleLocator, LinearLocator
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+import matplotlib.ticker as ticker
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -122,55 +129,105 @@ def multi_plot(db_path, select, x_key, shade, saturate, padding=4):
 
 	#GET FIRST AND LAST DATE
 	dates = get_dates(reference_df, [shade, saturate])
-	start = min(dates) - timedelta(days=padding)
-	end = max(dates) + timedelta(days=padding)
+	window_start = min(dates) - timedelta(days=padding)
+	window_end = max(dates) + timedelta(days=padding)
 
 	#create generic plotting dataframe
 	x_vals = list(set(reference_df[x_key]))
-	datetime_index = [i for i in perdelta(start,end,timedelta(days=1))]
+	datetime_index = [i for i in perdelta(window_start,window_end,timedelta(days=1))]
 
 	df = pd.DataFrame(index=datetime_index, columns=x_vals)
 	df = df.fillna(0)
 
 	#set plotting params
-	fig, ax = plt.subplots(figsize=df.shape[0] , facecolor='#eeeeee', tight_layout=True)
-	im = ax.pcolorfast(rule, cmap=cMap)
+	cMap = add_grey(cm.viridis, 0.9)
+	fig, ax = plt.subplots(figsize=df.shape , facecolor='#eeeeee', tight_layout=True)
 
 	#populate frames
-	for entry in shade[1:]:
-		df_ = df.copy()
+	for entry in shade:
+		df_ = df.copy(deep=True)
 		for x_val in x_vals:
 			if isinstance(entry, dict):
 				for key in entry:
 					filtered_df = reference_df[(reference_df[key] == entry[key][0])&(reference_df[x_key] == x_val)]
 					start = list(set(filtered_df[entry[key][1]]))[0]
-					if len(entry) == 3:
-						end = list(set(filtered_df[entry[key][1]]))[0]
-						active_dates = [i for i in perdelta(start,end,timedelta(days=1))]
-					df_.set_value(start, x_val, 1)
+					if len(entry[key]) == 3:
+						end = list(set(filtered_df[entry[key][2]]))[0]
+						active_dates = [i for i in perdelta(start,end+timedelta(days=1),timedelta(days=1))]
+						df_.set_value(active_dates, x_val, 1)
+					else:
+						df_.set_value(start, x_val, 1)
 		print df_
-		plt.pcolor(df_)
-		plt.yticks(np.arange(0.5, len(df.index), 1), df_.index)
-		plt.xticks(np.arange(0.5, len(df.columns), 1), df_.columns)
-		# plt.show()
+		# plt.pcolor(df_)
+		im = ax.pcolorfast(df_.T, cmap=add_grey(cm.gray_r, 0.8), alpha=.5)
+		plt.hold(True)
 
+	#populate frames
+	for entry in saturate:
+		df_ = df.copy(deep=True)
+		for x_val in x_vals:
+			if isinstance(entry, dict):
+				for key in entry:
+					filtered_df = reference_df[(reference_df[key] == entry[key][0])&(reference_df[x_key] == x_val)]
+					start = list(set(filtered_df[entry[key][1]]))[0]
+					print start
+					if len(entry[key]) == 3:
+						end = list(set(filtered_df[entry[key][2]]))[0]
+						active_dates = [i for i in perdelta(start,end,timedelta(days=1))]
+						df_.set_value(active_dates, x_val, 5)
+						print end
+					else:
+						df_.set_value(start, x_val, 1)
+			elif isinstance(entry, str):
+				filtered_df = reference_df[reference_df[x_key] == x_val]
+				active_dates = list(set(filtered_df[entry]))
+				df_.set_value(active_dates, x_val, 1)
+		# print df_
+		# plt.pcolor(df_)
+		im = ax.pcolorfast(df_.T, cmap=cm.PuRd, alpha=.5)
+		plt.hold(True)
 
-	# for entry in shade:
-	# 	if isinstance(entry, str):
-	# 		dates.append(list(set(mydf[entry])))
-	# 	if isinstance(entry, dict):
-	# 		for key in entry:
-	# 			filtered_df = mydf[mydf[key] == entry[key][0]]
-	# 			for col in entry[key][1:]:
-	# 				dates.append(list(set(filtered_df[col])))
+	#place and null major ticks (we still need them for the grid)
+	ax.xaxis.set_major_locator(LinearLocator(len(df_.index)+1))
+	ax.xaxis.set_major_formatter(ticker.NullFormatter())
+	ax.yaxis.set_major_locator(LinearLocator(len(df_.columns)+1))
+	ax.yaxis.set_major_formatter(ticker.NullFormatter())
 
+	#place and format minor ticks (used to substitute for centered major tick labels)
+	ax.xaxis.set_minor_locator(ticker.IndexLocator(1,0.5))
+	ax.xaxis.set_minor_formatter(ticker.FixedFormatter(df_.index))
+	ax.yaxis.set_minor_locator(ticker.IndexLocator(1,0.5))
+	ax.yaxis.set_minor_formatter(ticker.FixedFormatter(df_.columns))
 
+	#remove all major tick lines
+	# plt.setp(ax.get_xticklines(),visible=False)
 
-	#get shade declarative_base
-	# for entry in shade:
-	# 	if isinstance(entry, str):
-	# 		filtered_df = mydf[mydf[key] == ]
+	#remove bottom and top small tick lines
+	plt.tick_params(axis='x', which='both', bottom='off', top='off', left='off', right='off')
+	plt.tick_params(axis='y', which='both', bottom='off', top='off', left='off', right='off')
 
+	#Set only 7th minor tick label to visible
+	for label in ax.xaxis.get_minorticklabels():
+		label.set_visible(False)
+	for label in ax.xaxis.get_minorticklabels()[::7]:
+		label.set_visible(True)
+	for label in ax.yaxis.get_minorticklabels():
+		label.set_visible(False)
+	for label in ax.yaxis.get_minorticklabels():
+		label.set_visible(True)
+
+	#Rotate ytick labels
+	plt.setp(ax.xaxis.get_minorticklabels(), rotation=90)
+
+	# create grid
+	ax.xaxis.grid(True, which='major', color='1', linestyle='-')
+	ax.yaxis.grid(True, which='major', color='1', linestyle='-')
+
+	#delete all spines
+	for spine in ["right", "left", "top", "bottom"]:
+		ax.spines[spine].set_visible(False)
+
+	plt.ylabel(" ".join(x_key.split("_")).replace("id","ID"))
 
 	session.close()
 	engine.dispose()
@@ -215,8 +272,9 @@ def test(db_path):
 if __name__ == '__main__':
 	# argh.dispatch_command(add_generic)
 	select = [["Animal","treatments"],["FMRIMeasurement"],["TreatmentProtocol"],["Treatment","start_date","2015,11,11","2015,11,10"]]
-	shade = [{"TreatmentProtocol_code":["chrFlu","Treatment_start_date","Treatment_start_date"]},{"TreatmentProtocol_code":["acFlu","Treatment_start_date"]}]
+	shade = [{"TreatmentProtocol_code":["chrFlu","Treatment_start_date","Treatment_end_date"]},{"TreatmentProtocol_code":["acFlu","Treatment_start_date"]}]
 	multi_plot("~/meta.db", select, "Animal_id", shade=shade, saturate=["FMRIMeasurement_date"])
+	plt.show()
 	# simple_query("~/meta.db", "FMRIMeasurement", [{"animal_id":"Animal:id_eth.4011"},{"animal_id":"Animal:id_eth.4009"}])
 	# add_animal("~/animal.db", 4011, 4, "f", "2L", id_uzh="M2760", cage_uzh="570971")
 	# test("~/meta.db")
