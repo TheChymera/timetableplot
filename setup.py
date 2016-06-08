92a8944d89fa10bb5e202c3ebfb2adbea753c630
Horea Christian <horea.christ@yandex.com>
2016-06-08 04:54:49 +0200

    completed name migration

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-	name="timeplot",
+	name="timetableplot",
 	version="",
 	description = "Create overview figures of timetables",
 	author = "Horea Christian",
 	author_email = "h.chr@mail.ru",
-	url = "https://github.com/TheChymera/timeplot",
+	url = "https://github.com/TheChymera/timetableplot",
 	keywords = ["plot", "schedule", "time", "timetable"],
 	packages = find_packages("."),
 	install_requires = [],
