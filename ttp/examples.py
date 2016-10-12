b84aa821bf21e22d29a22cfacdd05c003c05aed3
Horea Christian <horea.christ@yandex.com>
2016-10-12 10:59:07 +0200

    Updated IP cohort example

--- ttp/examples.py
+++ ttp/examples.py
@@ -34,24 +34,24 @@ def dwcohort2():
 	filters = [["Cage_Treatment","start_date","2016,5,19,23,5"]]
 
 	# setting outerjoin to true will indirectly include controls
-	reference_df = get_df("~/syncdata/meta.db",col_entries=col_entries, join_entries=join_entries, filters=filters, outerjoin=True) # setting outerjoin to true will indirectly include controls
+	reference_df = get_df("~/syncdata/meta.db",col_entries=col_entries, join_entries=join_entries, filters=filters, outerjoin=True)
+	print(reference_df)
 
 	timetable.multi_plot(reference_df, "Animal_id", shade=["FMRIMeasurement_date"], saturate=saturate)
 	plt.show()
 
 def ipcohort():
 	saturate = [
-		{"TreatmentProtocol_code":["cFluIP","Treatment_start_date","Treatment_end_date"]},
-		{"TreatmentProtocol_code":["aFluIV","Treatment_start_date"]},
-		{"TreatmentProtocol_code":["aFluSC","Treatment_start_date"]}
+		{"Animal_TreatmentProtocol_code":["cFluIP","Animal_Treatment_start_date","Animal_Treatment_end_date"]},
+		{"Animal_TreatmentProtocol_code":["aFluIV","Animal_Treatment_start_date"]},
 		]
 
-	col_entries=[("Animal","id"),("Treatment",),("FMRIMeasurement",),("TreatmentProtocol","code")]
-	join_entries=[("Animal.treatments",),("FMRIMeasurement",),("Treatment.protocol",)]
+	col_entries=[("Animal","id"),("FMRIMeasurement",),("Animal","Treatment",""),("Animal","TreatmentProtocol","code")]
+	join_entries=[("Animal.treatments",),("FMRIMeasurement",),("Animal_Treatment","Animal.treatments"),("Animal_TreatmentProtocol","Animal_Treatment.protocol")]
 	filters = [["Treatment","start_date","2015,11,11"]]
 
 	# setting outerjoin to true will indirectly include controls
-	reference_df = get_df("~/syncdata/meta.db",col_entries=col_entries, join_entries=join_entries, filters=filters, outerjoin=True) 
+	reference_df = get_df("~/syncdata/meta.db",col_entries=col_entries, join_entries=join_entries, filters=filters, outerjoin=True)
 
 	timetable.multi_plot(reference_df, "Animal_id", shade=["FMRIMeasurement_date"], saturate=saturate)
 	plt.show()
